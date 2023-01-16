0207ecc14c4a007ac6194762e159ab637d7fa5be
Michal Hoffman <michal.z.hoffman@gmail.com>
2023-01-16 15:45:25 +0000

    Adding Meals List

---
+++ src/components/Meals/MealsSummary.js
@@ -0,0 +1,15 @@
+import classes from './MealsSummary.module.css'
+
+const MealsSummary = () => {
+  return (
+    <section className={classes.summary}>
+      <h2>Delicious Food</h2>
+      <p>
+        Choose your favorite meal from our broad selection of available meals
+        and enjoy a delicious food.
+      </p>
+    </section>
+  )
+}
+
+export default MealsSummary
