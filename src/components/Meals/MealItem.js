2bcaa9e050b049f4c29aba7a191df065454fc119
Michal Hoffman <michal.z.hoffman@gmail.com>
2023-02-08 13:09:51 +0000

    Chnaging cart display and replacing $ with £

--- src/components/Meals/MealItem.js
+++ src/components/Meals/MealItem.js
@@ -6,7 +6,7 @@ import classes from './MealItem.module.css'
 const MealItem = (props) => {
   const cartCtx = useContext(CartContext)
 
-  //const price = `$${props.price.toFixed(2)}`
+  const price = `£${props.price.toFixed(2)}`
 
   const addToCartHandler = (amount) => {
     cartCtx.addIteam({
@@ -21,7 +21,7 @@ const MealItem = (props) => {
       <div>
         <h3>{props.name}</h3>
         <div className={classes.description}>{props.description}</div>
-        <div className={classes.price}>£{props.price}</div>
+        <div className={classes.price}>{price}</div>
       </div>
       <div>
         <MealItemForm onAddToCart={addToCartHandler} id={props.id} />
